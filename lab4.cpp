8e82a995e307c62bc1c5abc6691c9e765e59485c
Dan <daniyar.burakaev@mail.ru>
2019-10-30 15:09:21 +0300

    multiple pthread using try

--- lab4.cpp
+++ lab4.cpp
@@ -9,10 +9,14 @@ void* my_foo(void * d) {
 }
 
 int main(int argv, char* argc[]) {
+	int n = 10;
 	void* data = NULL;
-	pthread_t thread;
+	pthread_t* thread = new pthread_t[n];
 	
-	pthread_create(&thread, NULL, my_foo, data);
-	pthread_join(thread, NULL);
+	for(int i = 0; i < n; ++i) {
+		pthread_create(&thread[i], NULL, my_foo, data);
+		pthread_join(thread[i], NULL);
+	}
+	
 	return 0;
 }
